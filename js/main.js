b5c83e804bb5d1aeb615d5f2807b4adcf4f9fc36
doorller <39507412+doorller@users.noreply.github.com>
2023-03-23 11:43:16 +0800

    feat:ios时间兼容

--- js/main.js
+++ js/main.js
@@ -266,7 +266,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("2023-05-01 00:00:00");
+		var endTime = new Date("2023/05/01 00:00:00");
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
